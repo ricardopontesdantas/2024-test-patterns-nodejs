378993664a68014cd36a7450f3ceca735d7cf29c
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-15 06:31:45 -0300

    create invalid header test case

--- mock/src/file.test.js
+++ mock/src/file.test.js
@@ -8,4 +8,14 @@ const File = require("./file");
     const expected = new Error("the content length is invalid");
     assert.rejects(result, expected, "it should rejects if the file is empty");
   }
+  {
+    const filePath = "./mocks/invalid-header.csv";
+    const result = File.csvToJson(filePath);
+    const expected = new Error(error.FILE_FIELDS_ERROR_MESSAGE);
+    assert.rejects(
+      result,
+      expected,
+      "it should rejects if the header is invalid"
+    );
+  }
 })();
