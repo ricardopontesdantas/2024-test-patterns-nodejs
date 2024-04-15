e0436596bba641260769779ddde7f476da7e0942
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-15 06:34:11 -0300

    create header validation

--- mock/src/file.js
+++ mock/src/file.js
@@ -1,17 +1,33 @@
 const { readFile } = require("fs/promises");
+const error = require("./constants");
+const DEFAULT_OPTIONS = {
+  maxLines: 3,
+  fields: ["id", "name", "profession", "age"],
+};
 
 class File {
   static async csvToJson(filePath) {
     const content = await readFile(filePath, "utf-8");
     const validation = this.isValid(content);
     if (!validation.valid) throw new Error(validation.error);
+    return this.parseCsvToJson(content);
   }
 
-  static isValid(csvString) {
+  static isValid(csvString, options = DEFAULT_OPTIONS) {
     const [header, ...fileWithoutHeader] = csvString.split(/\r?\n/);
+    const isHeaderValid = header === options.fields.join(",");
     if (!fileWithoutHeader.length) {
       return {
-        error: "the content length is invalid",
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
         valid: false,
       };
     }
