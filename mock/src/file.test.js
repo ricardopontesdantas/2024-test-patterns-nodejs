ec864d6cd2f342df7e8099a2c538cbd225de1e01
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-15 06:35:40 -0300

    create invalid five items test case

--- mock/src/file.test.js
+++ mock/src/file.test.js
@@ -1,17 +1,28 @@
 const assert = require("assert");
 const File = require("./file");
+const error = require("./constants");
 
 (async () => {
   {
     const filePath = "./mocks/invalid-empty-file.csv";
     const result = File.csvToJson(filePath);
-    const expected = new Error("the content length is invalid");
+    const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
     assert.rejects(result, expected, "it should rejects if the file is empty");
   }
   {
     const filePath = "./mocks/invalid-header.csv";
     const result = File.csvToJson(filePath);
     const expected = new Error(error.FILE_FIELDS_ERROR_MESSAGE);
+    assert.rejects(
+      result,
+      expected,
+      "it should rejects if the header is invalid"
+    );
+  }
+  {
+    const filePath = "./mocks/invalid-five-items.csv";
+    const result = File.csvToJson(filePath);
+    const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
     assert.rejects(
       result,
       expected,
