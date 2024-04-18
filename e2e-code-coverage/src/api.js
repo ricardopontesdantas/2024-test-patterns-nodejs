7a8c4e5fb38ca672ee94e32f3ae1e925c32c3ebf
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-18 06:37:03 -0300

    create a simple server with three routes

---
+++ e2e-code-coverage/src/api.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const { once } = require("events");
+const DEFAULT_USER = {
+  username: "Peter",
+  password: "123456",
+};
+const routes = {
+  "/:get": function (request, response) {
+    response.write("home page");
+    return response.end();
+  },
+  "/contact:get": function (request, response) {
+    response.write("contact page");
+    return response.end();
+  },
+  "/login:post": async function (request, response) {
+    const user = JSON.parse(await once(request, "data"));
+    const toLower = (text) => text.toLowerCase();
+    if (
+      toLower(user.username) !== toLower(DEFAULT_USER.username) ||
+      user.password !== DEFAULT_USER.password
+    ) {
+      response.writeHead(401);
+      response.write("login failed");
+      return response.end();
+    }
+    response.write("login successfully");
+    return response.end();
+  },
+  default: function (request, response) {
+    response.writeHead(404);
+    response.write("not found");
+    return response.end();
+  },
+};
+
+function handler(request, response) {
+  const { method, url } = request;
+  const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`;
+  const chosen = routes[routeKey] || routes.default;
+  return chosen(request, response);
+}
+
+const app = http
+  .createServer(handler)
+  .listen(3000, () => console.log("running at 3000"));
