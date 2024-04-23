918e1b8c13adbafc4ea4ac7ecbc562121e3cc325
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-23 06:41:16 -0300

    create test for the method that get the first id from car ids

--- project-tdd-bdd/test/unitTests/carService.test.js
+++ project-tdd-bdd/test/unitTests/carService.test.js
@@ -4,6 +4,9 @@ const { join } = require("path");
 const { createSandbox } = require("sinon");
 const CarService = require("../../src/service/carService");
 const carsDatabase = join(__dirname, "..", "..", "database", "cars.json");
+const mocks = {
+  carCategory: require("../mocks/valid-car-category"),
+};
 
 describe("CarService Suite Tests", () => {
   let carService;
@@ -21,5 +24,17 @@ describe("CarService Suite Tests", () => {
     const input = [0, 1, 2, 3, 4];
     const output = carService.getRandomPositionFromArray(input);
     expect(output).to.be.gte(0).and.lt(input.length);
+  });
+
+  it("sould choose the first id from carIds in carCategory", () => {
+    const input = mocks.carCategory;
+    const carIndex = 0;
+    sinon
+      .stub(carService, carService.getRandomPositionFromArray.name)
+      .returns(0);
+    const output = carService.chooseRandomCar(input);
+    const expected = input.carIds[carIndex];
+    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok;
+    expect(output).to.be.equal(expected);
   });
 });
