b97eab197c4a12f1432f27e5426b9129cd8f07e3
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-23 06:39:49 -0300

    require sinon in test file

--- project-tdd-bdd/test/unitTests/carService.test.js
+++ project-tdd-bdd/test/unitTests/carService.test.js
@@ -1,15 +1,21 @@
 const { describe, it, before } = require("mocha");
 const { expect } = require("chai");
 const { join } = require("path");
+const { createSandbox } = require("sinon");
 const CarService = require("../../src/service/carService");
 const carsDatabase = join(__dirname, "..", "..", "database", "cars.json");
 
 describe("CarService Suite Tests", () => {
   let carService;
+  let sinon;
 
   before(() => {
     carService = new CarService({ cars: carsDatabase });
   });
+
+  beforeEach(() => (sinon = createSandbox()));
+
+  afterEach(() => sinon.restore());
 
   it("should retrieve a random position from an array", () => {
     const input = [0, 1, 2, 3, 4];
