9d848d43d52d644a9384e415f6600ae23991d6f2
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-24 05:27:04 -0300

    implement the calculateFinalPrice method

--- project-tdd-bdd/src/service/carService.js
+++ project-tdd-bdd/src/service/carService.js
@@ -1,8 +1,14 @@
+const Tax = require("../entities/tax");
 const CarRepository = require("../repository/carRepository");
 
 class CarService {
   constructor({ cars }) {
     this.carRepository = new CarRepository({ file: cars });
+    this.taxesBasedOnAge = Tax.taxesBasedOnAge;
+    this.currencyFormat = new Intl.NumberFormat("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
   }
 
   getRandomPositionFromArray(list) {
@@ -20,6 +26,17 @@ class CarService {
     const carId = this.chooseRandomCar(carCategory);
     const car = await this.carRepository.find(carId);
     return car;
+  }
+
+  async calculateFinalPrice(carCategory, customer, numberOfDays) {
+    const { age } = customer;
+    const { price } = carCategory;
+    const { then: tax } = this.taxesBasedOnAge.find(
+      (tax) => age >= tax.from && age <= tax.to
+    );
+    const finalPrice = price * tax * numberOfDays;
+    const formattedPrice = this.currencyFormat.format(finalPrice);
+    return formattedPrice;
   }
 }
 
