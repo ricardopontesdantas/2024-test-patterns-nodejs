078ab70550023d5ecb5c24ca91e6d22156c32413
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2024-04-25 05:32:44 -0300

    implement test for default route

---
+++ project-tdd-bdd/test/e2eTests/api.test.js
@@ -0,0 +1,17 @@
+const { describe, it, before, after } = require("mocha");
+const supertest = require("supertest");
+
+describe("API Suite Test", () => {
+  let app;
+  before((done) => {
+    app = require("./../../src/api");
+    app.once("listening", done);
+  });
+  after((done) => app.close(done));
+  describe("get:non-existing", () => {
+    it("should request a non-existing url and return http status 404", async () => {
+      await supertest(app).get("/non-existing").expect(404);
+    });
+  });
+});
+
